c93f470dc1b8f88beeec2a800f97aad6d1e494dd
HaCsO <48949815+HaCsO@users.noreply.github.com>
2020-01-20 19:06:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 clear
 echo "--------------------"
 echo "|     Кто ты ?     |"
@@ -16,6 +15,7 @@ then
 	pkg install python3-pip
 	pip3 install requests
 	pip3 install colorama
+    pip3 install asyncio
 	cp ~/spymer/spammer.py /data/data/com.termux/files/usr/bin/spymer
 	dos2unix /data/data/com.termux/files/usr/bin/spymer
 	chmod 777 /data/data/com.termux/files/usr/bin/spymer
@@ -31,6 +31,7 @@ else
 			apt install python3 python3-pip
 			pip3 install requests
 			pip3 install colorama
+            pip3 install asyncio
 			cp ~/spymer/spammer.py /usr/bin/spymer
 			dos2unix /usr/bin/spymer
 			chmod 777 /usr/bin/spymer
@@ -40,4 +41,3 @@ else
 		echo "Некорректный ввод"
 	fi
 fi
-
